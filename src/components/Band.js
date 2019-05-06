af2afa45147a24010a3c51ea9e0f75afb5f74708
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-06 17:04:07 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -5,7 +5,7 @@ class Band extends Component {
   render() {
     return(
       <div>
-        <span>{this.props.band}</span>
+        <span>{this.props.band}</span><button onClick={() => this.props.delete(this.props.band.id)}>DELETE</button>
       </div>
     );
   }
