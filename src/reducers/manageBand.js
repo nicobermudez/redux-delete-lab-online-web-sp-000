7acb1c6d15f43a5d12720a4bba13618f596243b4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-06 17:04:48 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,10 @@
 export default function manageBand(state = {bands: []}, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
       return { ...state, bands: [...state.bands, action.name] }
-
+      
+    case 'DELETE_BAND':
+      return {}
     default:
       return state;
   }
